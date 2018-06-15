ec752cbd926866c987a32a7febcda57141492c9f
Finn Frankis <20FinnF@students.harker.org>
2018-06-14 22:26:37 -0700

    Removed unused commented code

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -16,23 +16,3 @@ $(document).ready(function(){
         $("#day5List").toggle(1000);
     });
 });
-/*
-    $("#hWeek1").click(function(){
-            $("#week1").toggle(500);
-            var $s = $("#hWeek1").not($(arrow));
-            $("hWeek1").text($s);
-    });
-    $("#hWeek2").click(function(){
-            $("#week2").toggle(500);
-       
-        
-    });
-    $("#hWeek3").click(function(){
-            $("#week3").toggle(500);
-       
-
-    });*/
-    
-    /*$("div:not(#main)").click(function(){
-        $(this).toggle();
-    });*/
